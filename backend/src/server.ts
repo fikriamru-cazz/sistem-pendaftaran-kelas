621da848e02d79665e71a58f289d04c6f73c27a0
fikriamru-cazz <fikriamru@gmail.com>
2025-10-04 20:15:52 +0700

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,6 +5,6 @@ const prisma = new PrismaClient();
  const PORT = process.env.PORT || 3001;
   const HOST = process.env.NODE_ENV === 'production' ? '0.0.0.0' : 'localhost';
 
-app.listen(Number(PORT), HOST, () => {
-    console.log(Server berjalan di http://${HOST}:${PORT});
-  });
+ app.listen(Number(PORT), HOST, () => {
+        console.log(Server berjalan di http://${HOST}:${PORT});
+      });
